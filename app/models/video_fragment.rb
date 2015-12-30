f474c6e6703e4b29a55592c66427d232ab61fad2
cometman <csbuiss@gmail.com>
2015-12-29 21:01:04 -0600

    video fragment added

--- app/models/video_fragment.rb
+++ app/models/video_fragment.rb
@@ -1,6 +1,12 @@
 class VideoFragment < ActiveRecord::Base
+  after_create :encode_fragment
+  
   has_attached_file :fragment
   validates_attachment_content_type :fragment, :content_type => ["video", "video\/MP2T", "video\/mp2t", "video/mp4", "video.MOV", "video/mpeg","video/mpeg4"]
 
   belongs_to :playlist
+
+  def encode_fragment
+    `mediafilesegmenter -t 5 #{self.fragment.path} -B #{self.playlist.id}_#{Time.now.to_i}`
+  end
 end
